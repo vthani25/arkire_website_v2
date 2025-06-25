3d2e42cc6537f1f032210e049772df4d7188dd59
vthani25 <vthanishkka@gmail.com>
2025-06-25 12:00:38 -0400

    changed to serveless vercel from node

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,9 +4,9 @@ import tailwind from '@astrojs/tailwind';
 import node from "@astrojs/node";
 import clerk from "@clerk/astro";
 import react from "@astrojs/react";
+import vercel from '@astrojs/vercel';
 
 export default defineConfig({
   integrations: [tailwind(), clerk(), react()],
-  adapter: node({ mode: "standalone" }),
-  output: "server",
+  adapter: vercel({}),
 });
