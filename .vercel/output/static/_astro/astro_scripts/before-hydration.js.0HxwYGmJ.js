1de61c6763b44c0493b51a775bb26d83989b33ae
vthani25 <vthanishkka@gmail.com>
2025-06-25 19:26:19 -0400

    added accessibility updates

---
+++ .vercel/output/static/_astro/astro_scripts/before-hydration.js.0HxwYGmJ.js
@@ -0,0 +1,1 @@
+import{r}from"../index.DB-AXAsh.js";await r({sdkMetadata:{version:"2.9.1",name:"@clerk/astro",environment:"production"}});
--- .vercel/output/static/_astro/astro_scripts/before-hydration.js.DfyabIJn.js
+++
@@ -1,1 +0,0 @@
-import{r}from"../index.CgyQzght.js";import"../chunk-PJK4I2LC.DIfsp0ZC.js";await r({sdkMetadata:{version:"2.9.1",name:"@clerk/astro",environment:"production"}});
